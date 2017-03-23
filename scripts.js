3809273cc1ddf38d998ef48522b9a772c64ee054
Rafal <lazlo102@gmail.com>
2017-03-23 21:30:56 +0100

    Funkcje

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+function getTriangleArea (a, h) {
+	var triangleArea = a*h/2
+		if ( a && h > 0 ) {
+		return triangleArea;
+		}
+		else if ( a && h <= 0 ) {
+		}
+			console.log('Nieprawidłowe dane')	
+	}
+console.log( getTriangleArea(10,6) )
+
+var triangle1Area = getTriangleArea(0, 15);
+	triangle2Area = getTriangleArea(15, 20);
+    triangle3Area = getTriangleArea(9, 5);
+
+console.log(triangle1Area)
+console.log(triangle2Area)
+console.log(triangle3Area)
