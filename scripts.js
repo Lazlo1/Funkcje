216736dbba84fe4090b80edbbde1bd89bbba52c7
Rafal <lazlo102@gmail.com>
2017-03-24 11:19:20 +0100

    Zmiany

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 
 function getTriangleArea (a, h) {
 	var triangleArea = a*h/2
-		if ( a && h > 0 ) {
+		if ( a > 0 && h > 0 ) {
 		return triangleArea;
 		}
-		else if ( a && h <= 0 ) {
+		else if ( a <= 0 && h <= 0 ) {
 		}
-			console.log('Nieprawidłowe dane')	
+			console.log('Nieprawidłowe dane');	
 	}
-console.log( getTriangleArea(10,6) )
+console.log( getTriangleArea(10,6) );
 
-var triangle1Area = getTriangleArea(0, 15);
-	triangle2Area = getTriangleArea(15, 20);
+var triangle1Area = getTriangleArea(0, 15),
+	triangle2Area = getTriangleArea(15, 20),
     triangle3Area = getTriangleArea(9, 5);
 
-console.log(triangle1Area)
-console.log(triangle2Area)
-console.log(triangle3Area)
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
